d7743742b782926e15f2a1d486c98493d9b7f3c9
Michael Wang <michael13491@gmail.com>
2018-06-04 20:46:21 +1200

    Adding timestamp package and documentations
    Explain a little bit about the server setup code,
    and how express works with middleware

--- web-api/server.js
+++ web-api/server.js
@@ -6,10 +6,18 @@ var mongoose = require('mongoose');
 //connect to the database, mongod will automatically create a new database if it does not exist
 var db = mongoose.connect('mongodb://localhost/baby-app');
 
+// "use" activates middleware, which means before a request reaches here, it goes through the middleware first
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+//prepare things to be json formatted before we handle it here
+app.use(bodyParser.json()); 
 
+//only want to work with array and strings, rejects anything else
+app.use(bodyParser.urlencoded({extended: false})); 
+
+
+app.get('/', function (request, response) {
+  response.send("into get req!");
+})
 
 app.listen(3000, function() {
   console.log("server running and listening to port 3000");
